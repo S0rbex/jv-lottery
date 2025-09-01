c1460d9a486b552824126a93eebb788c9a6ea2c4
S0rbex <vitalikmartunushun@gmail.com>
2025-09-01 19:21:40 +0300

    Fixing making new object in proccess

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(100) + 1; // від 1 до 100
+        int randomNumber = random.nextInt(MAX_NUMBER); // від 1 до 100
         return new Ball(randomColor, randomNumber);
     }
 }
